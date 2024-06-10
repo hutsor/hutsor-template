33e882c4d95cc8cb9a9516a6ab3262476d71d9ee
seungmkim <seungmkim@gmarket.com>
2024-06-10 10:47:52 +0900

    design: better reset css

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,7 @@
 export default function App() {
   return (
     <div className="main">
-      <h1>HUTSOR</h1>
-      <menu className="main-menu">
-        <li className="main-menu__item">
-          <a href="https://hutsor.github.io/hutsori">헛소리 제조기</a>
-        </li>
-      </menu>
+      <h1>Hello, World!</h1>
     </div>
   );
 }
